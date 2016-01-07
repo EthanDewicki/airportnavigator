db18d19f9f49513888d29116e5af66bbc8ae96bd
EthanDewicki <ejdewicki8@gmail.com>
2016-01-07 13:35:20 -0500

    Create viewcontroller.h

---
+++ viewcontroller.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AirportNavigator
+//
+//  Created by Ethan Dewicki on 12/26/15.
+//  Copyright © 2015 EJD Electronics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
+
+
+@end
